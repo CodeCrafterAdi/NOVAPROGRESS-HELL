d67404fbc2f4f98bc958caf7720f47db9797b148
Aditya Sharma <adityasharmabusiness1@gmail.com>
2025-12-17 15:40:36 +0530

    Update index.tsx

--- NOVAPROGRESS-HELL EDITION V7/index.tsx
+++ NOVAPROGRESS-HELL EDITION V7/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Page from './app/page';
+import { AuthProvider } from './context/AuthContext.tsx'; // ← NEW: Import AuthProvider
 // We import page from app/page which acts as the new App root.
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+throw new Error("Could not find root element to mount to");
 }
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <Page />
-  </React.StrictMode>
+<React.StrictMode>
+<AuthProvider> {/* ← NEW: Wrap Page in AuthProvider */}
+<Page />
+</AuthProvider>
+</React.StrictMode>
 );
